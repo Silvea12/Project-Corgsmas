22975cd7e53d7ae3d72b182345010915f1009b3a
Andrew Silver <andrew.silver0@gmail.com>
2016-12-16 18:21:21 +0100

    Corgi now can do a flip

--- Player.gd
+++ Player.gd
@@ -5,10 +5,13 @@ const MOVE_SPEED = 5
 const JUMP_VELOCITY = 15
 
 onready var sprite = get_node("Sprite")
+onready var animations = get_node("AnimationPlayer")
 
 var velocity = Vector2()
 var grounded = true
 var falling_through = false
+var trying_jump = false
+var air_jumps = 0
 
 func _fixed_process(delta):
 	if Input.is_action_pressed("right"):
@@ -20,7 +23,19 @@ func _fixed_process(delta):
 	else:
 		velocity.x = 0
 	
-	if Input.is_action_pressed("jump") and grounded:
+	var jumping = false
+	if Input.is_action_pressed("jump") and !trying_jump:
+		trying_jump = true
+		jumping = true
+	elif !Input.is_action_pressed("jump") and trying_jump:
+		trying_jump = false
+	
+	if jumping and (grounded or air_jumps < 1):
+		if !grounded:
+			air_jumps += 1
+		
+		if air_jumps == 1:
+			animations.play("Flip")
 		velocity.y = -JUMP_VELOCITY
 	
 	velocity.y += GRAVITY*delta
@@ -38,9 +53,13 @@ func _fixed_process(delta):
 	
 	var movement = move(velocity)
 	
+	
 	if is_colliding():
 		var n = get_collision_normal()
 		grounded = n.dot(Vector2(0, 1)) < -0.99
+		if grounded:
+			air_jumps = 0
+			trying_jump = false
 		movement = n.slide(movement)
 		velocity = n.slide(velocity)
 		move(movement)
