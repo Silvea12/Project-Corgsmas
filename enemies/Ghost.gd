6f0a89c39644fe74a589715999ed84f64e2539b5
Andrew Silver <andrew.silver0@gmail.com>
2016-12-18 11:49:53 +0100

    Added ghost fireball, increased present velocity, added fire delay for player

--- enemies/Ghost.gd
+++ enemies/Ghost.gd
@@ -9,12 +9,16 @@ const DEAD_DIST = 10
 
 onready var player = get_tree().get_nodes_in_group("player")[0]
 onready var sprite = get_node("AnimatedSprite")
+onready var fire_pos = get_node("FirePos")
+
+var projectile = preload("res://enemies/GhostProjectile.tscn")
 
 var velocity = Vector2()
 var health = 2
 var attacking = false
 var attack_anim = false
 var attack_timer = 0
+var fired = false
 
 func _hurt():
 	health -= 1
@@ -29,28 +33,30 @@ func _fixed_process(delta):
 	var angle = get_angle_to(player.get_global_pos())
 	var left_side = sin(angle) > 0
 	
+	if left_side != sprite.is_flipped_h():
+		var fpos = fire_pos.get_pos()
+		fpos.x *= -1
+		fire_pos.set_pos(fpos)
+	
 	sprite.set_flip_h(left_side)
+	
+	if attack_timer >= 4 and !attacking:
+		attacking = true
+		attack_timer = 0
+	if attacking:
+		if attack_timer >= 0.3:
+			if !attack_anim:
+				sprite.play("attack")
+			attack_anim = true
+		else:
+			sprite.stop()
+		return
 	
 	if cos(angle) < 0.3:
 		if left_side:
 			angle += PI/2
 		else:
 			angle -= PI/2
-	
-	if attack_timer >= 4 and !attacking:
-		attacking = true
-		attack_timer = 0
-	if attacking:
-		print("WAT")
-		if attack_timer >= 0.3:
-			if !attack_anim:
-				sprite.play("attack")
-				print("FWAP")
-			attack_anim = true
-		else:
-			sprite.stop()
-		return
-	
 	
 	var distance = (player.get_global_pos() - get_global_pos()).length()
 	var slowing = false
@@ -85,11 +91,23 @@ func _fixed_process(delta):
 
 func _process(delta):
 	if attack_anim:
-		if sprite.get_frame() == 4:
+		if sprite.get_frame() == 3:
+			if !fired:
+				fired = true
+				fire()
+		elif sprite.get_frame() == 4:
+			fired = false
 			attacking = false
 			attack_anim = false
 			sprite.set_animation("fly")
 		pass
+
+func fire():
+	var p = projectile.instance()
+	p.set_global_pos(fire_pos.get_global_pos())
+	p.add_collision_exception_with(self)
+	p.rotate(get_angle_to(player.get_global_pos()) + PI/2)
+	get_tree().get_root().add_child(p)
 
 func _ready():
 	set_process(true)
