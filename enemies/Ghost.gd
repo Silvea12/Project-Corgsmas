1713470376b1ec8a35b1cfa1c1ba674311c8e4dc
Andrew Silver <andrew.silver0@gmail.com>
2016-12-18 11:22:08 +0100

    Added ghost attacking logic (without actual attack)

--- enemies/Ghost.gd
+++ enemies/Ghost.gd
@@ -8,9 +8,13 @@ const APPROACH_DIST = 150
 const DEAD_DIST = 10
 
 onready var player = get_tree().get_nodes_in_group("player")[0]
+onready var sprite = get_node("AnimatedSprite")
 
 var velocity = Vector2()
 var health = 2
+var attacking = false
+var attack_anim = false
+var attack_timer = 0
 
 func _hurt():
 	health -= 1
@@ -20,15 +24,36 @@ func _hurt():
 	#get_node("CollisionShape2D").queue_free()
 
 func _fixed_process(delta):
+	attack_timer += delta
+	
 	var angle = get_angle_to(player.get_global_pos())
-	var distance = (player.get_global_pos() - get_global_pos()).length()
-	var slowing = false
+	var left_side = sin(angle) > 0
+	
+	sprite.set_flip_h(left_side)
 	
 	if cos(angle) < 0.3:
-		if sin(angle) > 0:
+		if left_side:
 			angle += PI/2
 		else:
 			angle -= PI/2
+	
+	if attack_timer >= 4 and !attacking:
+		attacking = true
+		attack_timer = 0
+	if attacking:
+		print("WAT")
+		if attack_timer >= 0.3:
+			if !attack_anim:
+				sprite.play("attack")
+				print("FWAP")
+			attack_anim = true
+		else:
+			sprite.stop()
+		return
+	
+	
+	var distance = (player.get_global_pos() - get_global_pos()).length()
+	var slowing = false
 	
 	if abs(distance - APPROACH_DIST) < DEAD_DIST:
 		slowing = true
@@ -58,6 +83,15 @@ func _fixed_process(delta):
 		if motion.length_squared() > 0.01:
 			move(motion)
 
+func _process(delta):
+	if attack_anim:
+		if sprite.get_frame() == 4:
+			attacking = false
+			attack_anim = false
+			sprite.set_animation("fly")
+		pass
+
 func _ready():
+	set_process(true)
 	set_fixed_process(true)
 	connect("hurt", self, "_hurt")
