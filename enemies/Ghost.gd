56d8d4e5f1b9172773b6792e564afafe06da8a30
Andrew Silver <andrew.silver0@gmail.com>
2016-12-18 10:51:15 +0100

    Added ghost

---
+++ enemies/Ghost.gd
@@ -0,0 +1,81 @@
+extends KinematicBody2D
+
+signal hurt
+
+const MAX_SPEED = 150
+const ACCELERATION = 30
+const APPROACH_DIST = 75
+
+onready var player = get_tree().get_nodes_in_group("player")[0]
+onready var sprite = get_node("AnimatedSprite")
+
+var velocity = Vector2()
+var spin_direction = false
+var swooping = false
+var time_swoop = 0
+var health = 2
+
+func _hurt():
+	health -= 1
+	if health == 0:
+		queue_free()
+	# Enable when we have death anim
+	#get_node("CollisionShape2D").queue_free()
+
+func _fixed_process(delta):
+	time_swoop += delta
+	var angle = get_angle_to(player.get_global_pos())
+	var speed_multiplier = 1
+	
+	if time_swoop > 4:
+		time_swoop = 0
+		swooping = true
+		sprite.set_animation("attack")
+		sprite.set_rot(angle+PI/2)
+	if swooping:
+		if time_swoop < 0.2:
+			speed_multiplier = 0
+			velocity = Vector2()
+		elif time_swoop > 2:
+			time_swoop = 0
+			swooping = false
+			sprite.set_animation("fly")
+			sprite.set_rot(0)
+		else:
+			speed_multiplier = 1.5
+	if !swooping:
+		if (player.get_global_pos() - get_global_pos()).length() < APPROACH_DIST:
+			if cos(angle) <= 0.2:
+				spin_direction = sin(angle) <= 0.1
+			
+			if spin_direction:
+				angle -= PI/2
+			else:
+				angle += PI/2
+		
+		#print(sin(angle))
+	
+	var new_dir = Vector2(0,1).rotated(angle)
+	velocity += new_dir*ACCELERATION*speed_multiplier
+	if velocity.length() > MAX_SPEED and !swooping:
+		velocity = velocity.normalized() * MAX_SPEED
+	
+	var motion = velocity*delta
+	
+	motion = move(motion)
+	
+	if is_colliding():
+		swooping = false
+		sprite.set_animation("fly")
+		sprite.set_rot(0)
+		time_swoop = 0
+		var n = get_collision_normal()
+		get_collider().emit_signal("hurt", get_collision_pos())
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
+		if motion.length_squared() > 0.01:
+			move(motion)
+
+func _ready():
+	set_fixed_process(true)
+	connect("hurt", self, "_hurt")
