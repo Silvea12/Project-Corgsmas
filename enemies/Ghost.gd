7e26303523a8be8ace6266011561bd38152cb91f
Andrew Silver <andrew.silver0@gmail.com>
2016-12-18 11:04:32 +0100

    Improved ghost behaviour

--- enemies/Ghost.gd
+++ enemies/Ghost.gd
@@ -4,15 +4,12 @@ signal hurt
 
 const MAX_SPEED = 150
 const ACCELERATION = 30
-const APPROACH_DIST = 75
+const APPROACH_DIST = 150
+const DEAD_DIST = 10
 
 onready var player = get_tree().get_nodes_in_group("player")[0]
-onready var sprite = get_node("AnimatedSprite")
 
 var velocity = Vector2()
-var spin_direction = false
-var swooping = false
-var time_swoop = 0
 var health = 2
 
 func _hurt():
@@ -23,54 +20,39 @@ func _hurt():
 	#get_node("CollisionShape2D").queue_free()
 
 func _fixed_process(delta):
-	time_swoop += delta
 	var angle = get_angle_to(player.get_global_pos())
-	var speed_multiplier = 1
+	var distance = (player.get_global_pos() - get_global_pos()).length()
+	var slowing = false
 	
-	if time_swoop > 4:
-		time_swoop = 0
-		swooping = true
-		sprite.set_animation("attack")
-		sprite.set_rot(angle+PI/2)
-	if swooping:
-		if time_swoop < 0.2:
-			speed_multiplier = 0
-			velocity = Vector2()
-		elif time_swoop > 2:
-			time_swoop = 0
-			swooping = false
-			sprite.set_animation("fly")
-			sprite.set_rot(0)
+	if cos(angle) < 0.3:
+		if sin(angle) > 0:
+			angle += PI/2
 		else:
-			speed_multiplier = 1.5
-	if !swooping:
-		if (player.get_global_pos() - get_global_pos()).length() < APPROACH_DIST:
-			if cos(angle) <= 0.2:
-				spin_direction = sin(angle) <= 0.1
-			
-			if spin_direction:
-				angle -= PI/2
-			else:
-				angle += PI/2
-		
-		#print(sin(angle))
+			angle -= PI/2
+	
+	if abs(distance - APPROACH_DIST) < DEAD_DIST:
+		slowing = true
+	elif distance < APPROACH_DIST:
+		angle += PI
 	
 	var new_dir = Vector2(0,1).rotated(angle)
-	velocity += new_dir*ACCELERATION*speed_multiplier
-	if velocity.length() > MAX_SPEED and !swooping:
-		velocity = velocity.normalized() * MAX_SPEED
+	if slowing:
+		if velocity.length() < ACCELERATION:
+			velocity = Vector2()
+		else:
+			velocity -= velocity.normalized()*ACCELERATION
+	else:
+		velocity += new_dir*ACCELERATION
+		if velocity.length() > MAX_SPEED:
+			velocity = velocity.normalized() * MAX_SPEED
 	
 	var motion = velocity*delta
 	
 	motion = move(motion)
 	
 	if is_colliding():
-		swooping = false
-		sprite.set_animation("fly")
-		sprite.set_rot(0)
-		time_swoop = 0
 		var n = get_collision_normal()
-		get_collider().emit_signal("hurt", get_collision_pos())
+		#get_collider().emit_signal("hurt", get_collision_pos())
 		motion = n.slide(motion)
 		velocity = n.slide(velocity)
 		if motion.length_squared() > 0.01:
